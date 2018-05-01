9c9efc5d9c0f0834605964a92cb21f8eb394d912
Fariza Khalamkhan <khalamkhanf@gmail.com>
2018-05-02 01:06:24 +0600

    herku

--- first_name/settings.py
+++ first_name/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = ')5dcsz#fnwm=#q!6)fg1*!e^0$^egv94sph!=t!z&y8i6bp(zx'
 #DEBUG = True
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1','localhost','fariza','herokuapp.com']
 
 
 # Application definition
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'first_name.urls'
@@ -124,16 +125,21 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-# The absolute path to the directory where collectstatic will collect static files for deployment.
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# Redirect to home URL after login (Default redirects to /accounts/profile/)
-LOGIN_REDIRECT_URL = '/'
-EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
+# Extra places for collectstatic to find static files.
+#STATICFILES_DIRS = (
+  #  os.path.join(BASE_DIR, 'static'),
+#)
+
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
